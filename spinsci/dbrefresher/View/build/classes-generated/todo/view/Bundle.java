1ad0e9fec9b18d6c676913db488886871652dda4
Karen Chai <karen@spinsci.com>
2015-04-29 03:00:08 +0800

    baseline commit

---
+++ spinsci/dbrefresher/View/build/classes-generated/todo/view/Bundle.java
@@ -0,0 +1,27 @@
+package todo.view;
+/** Localizable strings for {@link todo.view}. */
+@javax.annotation.Generated(value="org.netbeans.modules.openide.util.NbBundleProcessor")
+class Bundle {
+    /**
+     * @return <i>Tasks</i>
+     * @see TasksTopComponent
+     */
+    static String CTL_TasksAction() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_TasksAction");
+    }
+    /**
+     * @return <i>Safeway database refresher</i>
+     * @see TasksTopComponent
+     */
+    static String CTL_TasksTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "CTL_TasksTopComponent");
+    }
+    /**
+     * @return <i>This is a Tasks window</i>
+     * @see TasksTopComponent
+     */
+    static String HINT_TasksTopComponent() {
+        return org.openide.util.NbBundle.getMessage(Bundle.class, "HINT_TasksTopComponent");
+    }
+    private void Bundle() {}
+}
